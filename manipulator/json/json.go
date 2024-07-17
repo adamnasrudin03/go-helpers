804522fb3a88e4abaf661bc2f0e0614ef94f693f
Adam Nasrudin <adamnasrudin858@gmail.com>
2024-07-17 10:00:46 +0700

    SafeJsonMarshal

---
+++ manipulator/json/json.go
@@ -0,0 +1,20 @@
+package json
+
+import "encoding/json"
+
+// SafeJsonMarshal is a function that safely marshal a data to json.
+// if the Marshal process is failed, it will return the original Marshal result
+// and the error. This function is useful when you want to ensure that the data
+// is marshaled to json without any errors.
+func SafeJsonMarshal(data interface{}) ([]byte, error) {
+	resp, err := json.Marshal(data)
+	if err != nil {
+		return resp, err
+	}
+	var tempData interface{}
+	err = json.Unmarshal(resp, &tempData)
+	if err != nil {
+		return json.Marshal(data)
+	}
+	return resp, nil
+}
