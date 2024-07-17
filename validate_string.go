f5a4ddce35c5690e86126dbac41db0d9ce55b72b
Adam Nasrudin <adamnasrudin858@gmail.com>
2024-07-17 15:03:08 +0700

    IsPhoneNumberId

--- validate_string.go
+++ validate_string.go
@@ -2,7 +2,9 @@ package help
 
 import (
 	"net/mail"
+	"regexp"
 	"strconv"
+	"strings"
 
 	"github.com/google/uuid"
 )
@@ -53,4 +55,33 @@ func IsNumber(input string) bool {
 
 	// Check if the error is nil, indicating that the string is a valid number.
 	return err == nil
+}
+
+// IsPhoneNumberId checks if the given string is a valid Indonesian phone number.
+//
+// Parameters:
+// - input: The string to be validated.
+//
+// Returns:
+// - A boolean indicating whether the string is a valid phone number or not.
+func IsPhoneNumberId(input string) bool {
+	// The pattern to match the phone number. It should start with "+" or "62" or "0", followed by "8" and then 9 digits.
+	pattern := `^(\+62|62|0)(8)\d{9}$`
+
+	// Trim the input string to remove any non-digit characters. This is done to handle cases where the input string contains
+	// white spaces or any other non-digit characters.
+	phoneNumber := strings.TrimFunc(input, func(r rune) bool {
+		// Only keep digit characters and the "+" character, which is used to indicate the country code.
+		return !('0' <= r && r <= '9' || r == '+')
+	})
+
+	// Check if the phone number matches the pattern using the regexp.MatchString function.
+	// If there is an error during the matching process, return false.
+	match, err := regexp.MatchString(pattern, phoneNumber)
+	if err != nil {
+		return false
+	}
+
+	// Return the match result, indicating whether the phone number is valid or not.
+	return match
 }
